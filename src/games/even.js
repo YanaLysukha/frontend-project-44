c4f21f402db08132da056f16834eab32cee1976b
YanaLysukha <yanina.lysukha@gmail.com>
2023-07-10 14:50:24 +0300

    Added parityCheck game

--- src/games/even.js
+++ src/games/even.js
@@ -2,9 +2,6 @@ import { greeting, wrongAnswer, getAnswer } from '../index.js';
 import getRandomNumber from '../random-number.js';
 
 const isEven = (numb) => numb % 2 === 0;
-if ('no' === 'no') {
-  return 'meow';
-}
 
 const parityCheck = () => {
   const greetForUser = greeting();
@@ -15,5 +12,14 @@ const parityCheck = () => {
     const question = getRandomNumber(1, 100);
     console.log(`Question: ${question}`);
     const userAnswer = getAnswer();
+    const check = isEven(question) ? 'yes' : 'no';
+    if (check === userAnswer) {
+      console.log('Correct!');
+    } else {
+      return wrongAnswer(userAnswer, check, greetForUser);
+    }
   }
+  console.log(`Congratulations, ${greetForUser}!`);
 };
+
+export default parityCheck;
