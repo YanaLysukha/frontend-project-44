1bc14edacbf8db1e1f3bda1b59197ba889d38a6b
YanaLysukha <yanina.lysukha@gmail.com>
2023-07-16 16:57:47 +0300

    Added changes to index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,6 @@ const greeting = () => {
 
 const getAnswer = () => readlineSync.question('Your answer: ');
 
-const wrongAnswer = (wrongAnsw, correctAnsw, name) => {
-  console.log(`'${wrongAnsw}' is wrong answer ;(. Correct answer was '${correctAnsw}'.`);
-  console.log(`Let's try again, ${name}`);
-};
-
 const runGame = (task, round) => {
   const greetForUser = greeting();
   console.log(`${task}`);
@@ -28,7 +23,9 @@ const runGame = (task, round) => {
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return wrongAnswer(userAnswer, correctAnswer, greetForUser);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${greetForUser}`);
+      return;
     }
   }
   console.log(`Congratulations, ${greetForUser}!`);
